f3ec1e5bad5342fb0faee9ef3764fe40db2ebe23
afkhurana <31321265+afkhurana@users.noreply.github.com>
2023-09-10 14:26:46 -0400

    textractor.py: create StackExchangeTextractor

--- src/textractor.py
+++ src/textractor.py
@@ -1,3 +1,6 @@
+import pprint as pp
+import re
+
 from bs4 import BeautifulSoup
 
 from curler import SeleniumCurler
@@ -41,3 +44,26 @@ class Textractor:
         """
         page_source = self.urlget(url)
         return self.textract(page_source)
+    
+
+class StackExchangeTextractor(Textractor):
+    """Textractor for stackexchange.com."""
+
+    def textract(self, page_source: str) -> str:
+        soup = BeautifulSoup(page_source, 'html.parser')
+        text = []
+        # get title
+        question_header_element = soup.find('div', attrs={'id': 'question-header'})
+        text.append(question_header_element.get_text())
+        # get question/answer bodies
+        for post in soup.find_all('div', class_='js-post-body'):
+            text.append(re.sub(r'\n+', '\n', post.get_text().strip()))
+        return '\n\n'.join(text)
+    
+
+if __name__ == '__main__':
+    # test extraction
+    textractor = StackExchangeTextractor()
+    url = 'https://stackoverflow.com/questions/31324218/scikit-learn-how-to-obtain-true-positive-true-negative-false-positive-and-fal'
+    text = textractor(url)
+    print(text)
